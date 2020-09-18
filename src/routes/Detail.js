2911db70cd73e60d8a778d7f15bdac50072112cd
KenPark <kkum04@gmail.com>
2020-09-18 12:05:59 +0900

    상세 페이지 구현

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,34 @@
-export default () => 'Detail';
+//import React from 'react';
+import {gql} from 'apollo-boost';
+import {useParams} from 'react-router-dom';
+import {useQuery} from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+      movie(id: $id) {
+          id
+          title
+          medium_cover_image
+          description_intro
+      }
+  }
+`
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: {
+      id: parseInt(id)
+    }
+  });
+
+  if (loading) {
+    return 'Loading...'
+  }
+
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+
+  return '';
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {gql} from "apollo-boost"
 import {useQuery} from "@apollo/react-hooks"
+import Movie from "../component/Movie";
 
 const GET_MOVIES = gql`
     {
@@ -12,15 +13,16 @@ const GET_MOVIES = gql`
 `
 
 export default () => {
-  const {loading, error, data} = useQuery(GET_MOVIES)
-  if (loading) {
-    return 'loading...';
-  }
+  const {loading, data} = useQuery(GET_MOVIES)
 
-  if (data && data.movies) {
-    return data.movies.map(m => <h1>{m.id}</h1>)
-  }
-
-  return ""
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {!loading &&
+        data.movies &&
+        data.movies.map(m => <Movie key={m.id} id={m.id} />)
+      }
+    </>
+  )
 }
 
